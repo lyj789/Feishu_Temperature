ba1b3ec2a917b069b79f2841e4c744d9e4cd33a6
lyj789 <41284684+lyj789@users.noreply.github.com>
2021-04-26 18:56:08 +0800

    add feishu.py v1

---
+++ feishu.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium import webdriver
+import time
+import time
+while True:
+  time_now = time.strftime("%H:%M:%S", time.localtime())
+  if time_now == "11:11:11":
+    # profile_directory = r'C:\Users\DELL\AppData\Roaming\Mozilla\Firefox\Profiles\ufngooe2.default-release'
+    # profile = webdriver.FirefoxProfile(profile_directory)
+    # browser = webdriver.Firefox(firefox_profile=profile, executable_path=r'D:\GeckoDriver\geckodriver.exe')
+
+    option = webdriver.ChromeOptions()
+    option.add_argument(r'user-data-dir=C:\Users\DELL\AppData\Local\Google\Chrome\User Data')
+    browser=webdriver.Chrome(options=option)
+    browser.get('https://feishu.nankai.edu.cn/?appid=229')
+    time.sleep(10)
+    js = """
+    var q1=document.getElementById("q1");
+    var arr=q1.options;
+    var text=arr[3].value;
+    document.getElementById("q1").value = text ;
+    
+    var q16=document.getElementById("q16");
+    var arr2=q16.options;
+    var text2=arr2[3].value;
+    document.getElementById("q16").value = text2 ;
+    
+    var q17=document.getElementById("q17");
+    var arr3=q17.options;
+    var text3=arr3[3].value;
+    document.getElementById("q17").value = text3 ;
+    
+    var q13=document.getElementById("q13");
+    var arr4=q13.options;
+    var text4=arr4[1].value;
+    document.getElementById("q13").value = text4 ;
+    """
+    browser.execute_script(js)
+    time.sleep(10)
+    browser.quit()
+
+    time.sleep(60)
